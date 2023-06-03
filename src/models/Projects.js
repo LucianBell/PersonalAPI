7495acf4cd7e66f3bd5aab0b6cca5597e511c63b
Lucian Bellini <lucianbellini1@gmail.com>
2023-06-03 15:25:17 -0300

    Creating project schema

--- src/models/Projects.js
+++ src/models/Projects.js
@@ -1,1 +1,15 @@
 import mongoose from "mongoose";
+
+const projectsSchema = new mongoose.Schema({
+  id: { type: Number },
+  image: { type: String },
+  title: { type: String },
+  description: { type: String },
+  languages: [
+    {
+      name: String,
+      icon: String,
+    },
+  ],
+  codeLink: { type: String },
+});
