ae52d506d137d394310f821bec0f65b03a92a1ac
Lucian Bellini <lucianbellini1@gmail.com>
2023-06-04 01:30:29 -0300

    Adding delete and put functions to projects

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,4 +1,4 @@
-const { Project: projectSchema } = require("../models/Project");
+const { Project: projectSchema, Project } = require("../models/Project");
 
 const projectController = {
   create: async (req, res) => {
@@ -23,6 +23,58 @@ const projectController = {
     try {
       const projects = await projectSchema.find();
       res.status(200).json(projects);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  },
+  get: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const project = await projectSchema.findById(id);
+
+      if (!project) {
+        res.status(404).json({ message: "Project not found" });
+        return;
+      }
+      res.status(200).json(project);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const project = await projectSchema.findById(id);
+
+      if (!project) {
+        res.status(404).json({ message: "Project not found" });
+      }
+
+      const deletedProject = await projectSchema.findByIdAndDelete(id);
+      res
+        .status(200)
+        .json({ deletedProject, message: "Project deleted sucessfully" });
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const project = {
+        image: req.body.image,
+        title: req.body.title,
+        languages: req.body.languages,
+        codeLink: req.body.codeLink,
+      };
+
+      const updatedProject = await projectSchema.findByIdAndUpdate(id, project);
+
+      if (!updatedProject) {
+        res.status(404).json({ error: "Project not found" });
+      }
+
+      res.status(200).json({ project, message: "Project updated sucessfully" });
     } catch (error) {
       res.status(500).json({ error: error });
     }
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -9,4 +9,16 @@ router
 
 router.route("/projects").get((req, res) => projectController.getAll(req, res));
 
+router
+  .route("/projects/:id")
+  .get((req, res) => projectController.get(req, res));
+
+router
+  .route("/projects/:id")
+  .delete((req, res) => projectController.delete(req, res));
+
+router
+  .route("/projects/:id")
+  .put((req, res) => projectController.update(req, res));
+
 module.exports = router;
